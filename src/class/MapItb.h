e4b41b2b8c000833089a79b31ffccca218f5c249
Aurelia <aurellerua@ymail.com>
2015-03-14 22:42:19 +0700

    peta itb

---
+++ src/class/MapItb.h
@@ -0,0 +1,34 @@
+#ifndef MAPITB_H
+#define MAPITB_H
+
+#include "Object.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class MapItb {
+    public:
+        MapItb();
+        ~MapItb();
+        void Draw(FrameBuffer frame);
+        vector<Object> getGedung();
+
+    private:
+        Object *albar;
+        Object *altim;
+        Object *ccbar;
+        Object *cctim;
+        Object *lab5;
+        Object *lab6;
+        Object *lab7;
+        Object *lab8;
+        Object *tvst;
+        Object *oktagon;
+        Object *comlabs;
+        Object *pln;
+        Object *pau;
+        Object *perpus;
+};
+
+#endif // MAPITB_H
